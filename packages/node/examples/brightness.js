63ca601e5afd2a85af031668a5c9432810e2bc68
Julian Waller <git@julusian.co.uk>
2024-11-01 18:38:25 +0000

    wip: implement brightness

--- packages/node/examples/brightness.js
+++ packages/node/examples/brightness.js
@@ -1,34 +1,27 @@
 // @ts-check
-const { listStreamDecks, openStreamDeck } = require('../dist/index')
+import { listMXCreativeConsoleDevices, openMxCreativeConsole } from '../dist/index.js'
 
-;(async () => {
-	const devices = await listStreamDecks()
-	if (!devices[0]) throw new Error('No device found')
+const devices = await listMXCreativeConsoleDevices()
+if (!devices[0]) throw new Error('No device found')
 
-	const streamDeck = await openStreamDeck(devices[0].path)
+const device = await openMxCreativeConsole(devices[0].path)
 
-	// Fill it white so we can see the brightness changes
-	const buttonControls = streamDeck.CONTROLS.filter((control) => control.type === 'button')
-	for (const control of streamDeck.CONTROLS) {
-		if (control.type === 'button' && control.feedbackType !== 'none') {
-			streamDeck.fillKeyColor(control.index, 255, 255, 255).catch((e) => console.error('Fill failed:', e))
-		} else if (control.type === 'lcd-segment') {
-			const buffer = Buffer.alloc(control.pixelSize.width * control.pixelSize.height * 3).fill(
-				Buffer.from([255, 255, 255]),
-			)
-			streamDeck.fillLcd(control.id, buffer, { format: 'rgb' }).catch((e) => console.error('Fill lcd failed:', e))
-		}
+// Fill it white so we can see the brightness changes
+const buttonControls = device.CONTROLS.filter((control) => control.type === 'button')
+for (const control of device.CONTROLS) {
+	if (control.type === 'button' && control.feedbackType !== 'none') {
+		device.fillKeyColor(control.index, 255, 255, 255).catch((e) => console.error('Fill failed:', e))
 	}
+}
 
-	streamDeck.on('down', (control) => {
-		if (control.type !== 'button') return
+device.on('down', (control) => {
+	if (control.type !== 'button') return
 
-		const percentage = (100 / (buttonControls.length - 1)) * control.index
-		console.log(`Setting brightness to ${percentage.toFixed(2)}%`)
-		streamDeck.setBrightness(percentage).catch((e) => console.error('Set brightness failed:', e))
-	})
+	const percentage = (100 / (buttonControls.length - 1)) * control.index
+	console.log(`Setting brightness to ${percentage.toFixed(2)}%`)
+	device.setBrightness(percentage).catch((e) => console.error('Set brightness failed:', e))
+})
 
-	streamDeck.on('error', (error) => {
-		console.error(error)
-	})
-})()
+device.on('error', (error) => {
+	console.error(error)
+})
