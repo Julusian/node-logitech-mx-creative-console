5eb2e8dbbb04fda604969ca289c2628ffa790819
Julian Waller <git@julusian.co.uk>
2025-06-15 00:16:35 +0100

    fix: revert back to sofie tooling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,20 @@
-import { generateEslintConfig } from '@companion-module/tools/eslint/config.mjs'
+import { generateEslintConfig } from '@sofie-automation/code-standard-preset/eslint/main.mjs'
 
-const baseConfig = await generateEslintConfig({
-	enableTypescript: true,
-})
+const baseConfig = await generateEslintConfig({ enableTypescript: true })
 
 const customConfig = [
 	...baseConfig,
 
-	{
-		files: ['**/*.ts', '**/*.tsx'],
-		rules: {
-			'@typescript-eslint/consistent-type-imports': 'error',
-		},
-	},
+	{ files: ['**/*.ts', '**/*.tsx'], rules: { '@typescript-eslint/consistent-type-imports': 'error' } },
 
-	{
-		files: ['**/examples/*.js', '**/*.cjs'],
-		rules: {
-			'@typescript-eslint/no-require-imports': 'off',
-		},
-	},
+	{ files: ['**/examples/*.js', '**/*.cjs'], rules: { '@typescript-eslint/no-require-imports': 'off' } },
 	{
 		files: ['**/__tests__/**/*', '**/examples/**/*'],
-		rules: {
-			'n/no-extraneous-require': 'off',
-			'n/no-extraneous-import': 'off',
-		},
+		rules: { 'n/no-extraneous-require': 'off', 'n/no-extraneous-import': 'off' },
 	},
 	{
 		files: ['packages/webhid-demo/src/**/*'],
-		rules: {
-			'@typescript-eslint/no-require-imports': 'off',
-			'n/no-missing-import': 'off',
-		},
+		rules: { '@typescript-eslint/no-require-imports': 'off', 'n/no-missing-import': 'off' },
 	},
 ]
 
