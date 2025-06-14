580b3ac4b8434b4300f8eb0142d5be9cda34c19f
Julian Waller <git@julusian.co.uk>
2025-06-14 22:55:43 +0100

    chore: generate udev rules

---
+++ scripts/regenerate-udev.mjs
@@ -0,0 +1,43 @@
+/* eslint-disable n/no-extraneous-import */
+// @ts-check
+
+import fs from 'node:fs/promises'
+
+import { UdevRuleGenerator } from 'udev-generator'
+import { DEVICE_MODELS2, VENDOR_ID } from '@logitech-mx-creative-console/core'
+
+const generator = new UdevRuleGenerator()
+
+for (const model of Object.values(DEVICE_MODELS2)) {
+	for (const productId of model.productIds) {
+		if (productId >= 0xfff0) continue // Skip reserved product IDs
+		generator.addDevice(VENDOR_ID, productId)
+	}
+}
+
+const headlessStr = generator.generateFile({ mode: 'headless' })
+const desktopStr = generator.generateFile({ mode: 'desktop' })
+
+const rulesJSON = JSON.stringify(generator.rules, undefined, '\t')
+
+await fs.writeFile(
+	new URL('../packages/node/udev/50-logi-mx-creative-console-user.rules', import.meta.url),
+	desktopStr,
+	'utf8',
+)
+await fs.writeFile(
+	new URL('../packages/node/udev/50-logi-mx-creative-console-headless.rules', import.meta.url),
+	headlessStr,
+	'utf8',
+)
+
+await fs.writeFile(
+	new URL('../packages/webhid/udev/50-logi-mx-creative-console-user.rules', import.meta.url),
+	desktopStr,
+	'utf8',
+)
+
+await fs.writeFile(new URL('../packages/node/udev-generator-rules.json', import.meta.url), rulesJSON, 'utf8')
+await fs.writeFile(new URL('../packages/webhid/udev-generator-rules.json', import.meta.url), rulesJSON, 'utf8')
+
+console.log('Udev rules regenerated successfully!')
